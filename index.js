551c3b26c389f722b5eb14b56a3eb441a8ac3ef7
Kyle Lee <hello.kylelee@gmail.com>
2017-03-11 20:27:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 function getRepositories() {
   var username = document.getElementById('username').value;
   const req = new XMLHttpRequest();
-  req.addEventListener("load", showRepositories);
+  req.onload(showRepositories());
   req.open("GET", "https://api.github.com/users/" + username + "/repos");
   req.send();
 }
 
 function showRepositories() {
-  debugger; //it isn't getting to here... the link is valid, it almost seems like the GET request isn't completing or something.
   var returnedJSON = this.responseText;
   console.log(returnedJSON);
 }
