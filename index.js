d6cca08ba93ff41bd2a3cdead1d69d9aca55e45e
Kyle Lee <hello.kylelee@gmail.com>
2017-03-11 21:55:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+var rootURL = "https://api.github.com";
+
 function getRepositories() {
   var username = document.getElementById('username').value;
-  const req = new XMLHttpRequest();
-  req.onload(showRepositories());
-  req.open("GET", "https://api.github.com/users/" + username + "/repos");
+  var uri = rootURL + "/users/" + username + "/repos";
+  debugger; //check uri variable
+  var req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
+  req.open("GET", uri);
+  debugger; //check req.open
   req.send();
+  debugger;
+  // return false;
+  debugger;
 }
 
 function showRepositories() {
-  var returnedJSON = this.responseText;
-  console.log(returnedJSON);
+  var repositories = JSON.parse(this.responseText);
+  debugger; //check repositories variable
+  var repoList = `<ul>${repositories.map(repo => '<li><strong>' + repo.name + '</strong> - <a href="' + repo.html_url + '">' + repo.html_url + '</a></li>').join('')}</ul>`;
+  debugger;
+  document.getElementById('repositories').innerHTML = repoList;
+  debugger;
 }
